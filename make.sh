576e9df3a79eb041c29eb980a9d3446d1ef6e3e7
Leonardo Russo <info@lrusso.com>
2022-07-24 19:38:52 -0300

    Add files via upload

--- make.sh
+++ make.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-# creating the audio file
+# -------------------------------------------------------------------------------------------------------------------
+# CREATING THE AUDIO FILE
+# -------------------------------------------------------------------------------------------------------------------
 
 let audioCounter1=-1
 let audioDelayInMS=-1
 
+# CHECKING IF THERE IS ANY PREVIOUS AUDIO OUTPUT AND DELETING THE FILE
 if test -f "output.mp3";
   then
     rm output.mp3
 fi
 
+# CHECKING IF THERE IS AN AUDIO CONFIGURATION
 if test -f "input_audio.cfg";
   then
+
+    # WRITING THE FILE THAT WILL BE CALLING THE FFMPEG COMMAND FOR CREATING THE AUDIO FILE
     echo "ffmpeg \\" >output_audio.sh
 
+    # PASSING ALL THE AUDIO FILES AS INPUT PARAMETERS
     while IFS=, read -r field1 field2
       do
         echo "-i "$field1" \\" >>output_audio.sh
     done < input_audio.cfg
 
+    # WRITING THE AUDIO FILTER PARAMETER
     echo "-filter_complex \"\\" >>output_audio.sh
 
+    # PASSING EVERY DELAY OF EVERY AUDIO
     while IFS=, read -r field1 field2
       do
         audioDelayInMS=`echo $field2 1000 | awk '{print $1 * $2}'`
@@ -28,44 +37,52 @@ if test -f "input_audio.cfg";
         echo "["$audioCounter1"]adelay="$audioDelayInMS"|"$audioDelayInMS"[a"$audioCounter1"]; \\" >>output_audio.sh
     done < input_audio.cfg
 
+    # RESETTING THE AUDIO COUNTER
     audioCounter1=-1
 
+    # DECLARING ALL THE AUDIOS
     while IFS=, read -r field1 field2
       do
         audioCounter1=$((audioCounter1+1))
         echo "[a"$audioCounter1"]\\" >>output_audio.sh
     done < input_audio.cfg
 
+    # UPDATING THE AUDIO CONTAINER
     audioCounter1=$((audioCounter1+1))
 
+    # SETTING ALL THE AUDIOS THAT WILL BE USED
     echo "amix=inputs=$audioCounter1 \\" >>output_audio.sh
 
-
+    # WORKAROUND FOR THE AMIX FILTERS WHEN TRIES TO IMPLEMENT A FADE IN EFFECT IN THE NEW AUDIO
+    # AND WHEN MODIFIED THE VOLUME OF THE NEW AUDIO.
     echo ":duration=first:dropout_transition=99999999,volume=2.1\" \\" >>output_audio.sh
 
-
+    # SETTING THE OUTPUT AUDIO FILENAME
     echo "output_audio.mp3" >>output_audio.sh
 
-
+    # MAKING THE OUTPUT AUDIO SCRIPT EXECUTABLE
     chmod +x output_audio.sh
+
+    # RUNNING THE OUTPUT AUDIO SCRIPT
     ./output_audio.sh
 
 fi
 
-# creating the video file with the audio file (if exists)
+# -------------------------------------------------------------------------------------------------------------------
+# CREATING THE VIDEO FILE
+# -------------------------------------------------------------------------------------------------------------------
 
 let videoCounter1=0
 let videoCounter2=0
 let videoSlideDuration=0
 let videoDuration=0
-let videoMusicEnabled=0
+let videoAudioEnabled=0
 let videoTimeValue=0
 
-
-
+# WRITING THE FILE THAT WILL BE CALLING THE FFMPEG COMMAND FOR CREATING THE VIDEO FILE
 echo "ffmpeg \\" >output.sh
 
-
+# PASSING ALL THE IMAGE FILES
 while IFS=, read -r field1 field2
   do
     videoTimeValue=`echo $videoDuration+$field2 | awk '{print $1 + $2}'`
@@ -74,17 +91,17 @@ while IFS=, read -r field1 field2
     echo "-loop 1 -t "$videoTimeValue" -i "$field1" \\" >>output.sh
 done < input_video.cfg
 
-
+# CHECKING IF THERE IS AN AUDIO OUTPUT CREATED AND USING IT
 if test -f "output_audio.mp3";
   then
-    videoMusicEnabled=$((videoMusicEnabled+1))
+    videoAudioEnabled=$((videoAudioEnabled+1))
     echo "-i output_audio.mp3 \\" >>output.sh
 fi
 
-
+# ADDING THE VIDEO FILTER PARAMETER
 echo "-filter_complex \" \\" >>output.sh
 
-
+# PASSING ALL THE FADE IN EFFECT FOR EVERY SLIDE
 while IFS=, read -r field1 field2
   do
     if ((videoCounter2>0))
@@ -96,7 +113,7 @@ while IFS=, read -r field1 field2
     videoCounter2=$((videoCounter2+1))
 done < input_video.cfg
 
-
+# DECLARING ALL THE SLIDES
 for (( i=0; i<=$videoCounter1-2; i++ ))
   do
     if (($i==0));
@@ -112,40 +129,44 @@ for (( i=0; i<=$videoCounter1-2; i++ ))
       fi
 done
 
-
+# ENABLING THE VIDEO MAP
 echo "-map \"[v]\" \\" >>output.sh
 
-
-if ((videoMusicEnabled>0))
+# CHECKING IF THERE IS AUDIO AND ENABLING THE AUDIO MAP
+if ((videoAudioEnabled>0))
   then
    echo "-map "$((videoCounter1))":a \\" >>output.sh
 fi
 
-
+# SETTING THE VIDEO DURATION
 echo "-t "$videoDuration" \\" >>output.sh
+
+# SETTING THE OUTPUT VIDEO FILENAME
 echo "output.mp4" >>output.sh
 
-
+# CHECKING IF THERE IS ANY PREVIOUS VIDEO OUTPUT AND DELETING THE FILE
 if test -f "output.mp4";
   then
     rm output.mp4
 fi
 
-
+# MAKING THE OUTPUT VIDEO SCRIPT EXECUTABLE
 chmod +x output.sh
 ./output.sh
 
-
+# DELETING THE AUDIO OUTPUT SCRIPT
 if test -f "output_audio.sh";
   then
     rm output_audio.sh
 fi
 
+# DELETING THE AUDIO OUTPUT FILE
 if test -f "output_audio.mp3";
   then
     rm output_audio.mp3
 fi
 
+# DELETING THE VIDEO OUTPUT SCRIPT
 if test -f "output.sh";
   then
     rm output.sh
